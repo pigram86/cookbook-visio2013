4b17aea9d5f3b8e53ababbc82c45c4e87c8a6654
pigram86 <todd@toddpigram.com>
2014-06-17 11:29:19 -0400

    Initial Commit

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,8 @@ maintainer_email 'todd@toddpigram.com'
 license          'Apache 2.0'
 description      'Installs/Configures visio2013'
 long_description 'Installs/Configures visio2013'
-version          '0.1.0'
+version          '1.0.0'
+depends          "windows"
+
+supports 'windows'
 
